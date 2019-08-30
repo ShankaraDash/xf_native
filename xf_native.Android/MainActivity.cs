65a0cb746ec8246d36c43198f68a9b213c010547
Shankara Dash Shivagana <shankara@agora.io>
2019-08-29 23:19:37 -0700

    Added a layout to hold the Agora stream's video.

--- xf_native.Android/MainActivity.cs
+++ xf_native.Android/MainActivity.cs
@@ -17,12 +17,17 @@ namespace xf_native.Droid
     [Activity(Label = "xf_native", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        // We'll use this layout to render the Agora video stream to.
+        private View _layout;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.RemoteStream1);
+            _layout = FindViewById(Resource.Id.remote_stream1);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
